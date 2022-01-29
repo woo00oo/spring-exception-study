ee1718d6a66c61e5dc2b6b379e87accd378d6e7f
woo00oo <96woo94@naver.com>
2022-01-29 19:54:24 +0900

    API 예외처리 - HandlerExceptionResolver 활용

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,8 +1,10 @@
 package hello.exception.api;
 
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,10 @@ public class ApiExceptionController {
         if (id.equals("bad")) {
             throw new IllegalArgumentException("잘못된 입력 값");
         }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
         return new MemberDto(id, "hello " + id);
     }
 
